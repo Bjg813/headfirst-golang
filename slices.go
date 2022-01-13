41e21ded180bf06688762fd7e879bab6dec76ba5
Brennan Grout <bjg813@gmail.com>
2022-01-12 16:52:53 -0800

    added slice operator

--- slices.go
+++ slices.go
@@ -46,4 +46,32 @@ func main() {
 		5,
 	}
 	fmt.Println(primesLiteral[0], primesLiteral[1], primesLiteral[2])
+
+	// Slice operator - every slice is built on top of an underlying array.
+	// Slice operator takes 2 indexes, the first is the starting point of the array and the second ends but does not include the index of the array.
+	underlyingArray := [5]string{"a", "b", "c", "d", "e"}
+	slice1 := underlyingArray[0:3]
+	fmt.Println(slice1)
+	// [a b c]
+
+	i, j := 1, 4
+	slice2 := underlyingArray[i:j]
+	fmt.Println(slice2)
+	// [b c d]
+
+	// If you want a slice to include the last element of an underlying array, you specify a second index that's one beyond the end of the array.
+	slice3 := underlyingArray[0:5]
+	fmt.Println(slice3)
+	// [a b c d e]
+
+	// Slice operator has defaults for both the start and stop indexes.
+	// If you omit the start index, a value of 0 (the first element of the array)
+	slice4 := underlyingArray[:3]
+	fmt.Println(slice4)
+	// [a b c]
+
+	// If you omit the stop index, the remaining underlying array is included in the resulting slice.
+	slice5 := underlyingArray[1:]
+	fmt.Println(slice5)
+	// [b c d e]
 }
